cf42a31baaaa0928e4600647efece25287779c06
Chiheb Rahmouni <chihebrahmouni32@gmail.com>
2025-08-09 14:51:44 +0100

    feat: Enhance TabsComponent to accept custom styles for better flexibility

--- src/components/ui/tabs.component.tsx
+++ src/components/ui/tabs.component.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { ScrollView, TouchableOpacity } from "react-native";
+import { ScrollView, TouchableOpacity, ViewStyle } from "react-native";
 import { TextBody } from "./texts/Texts.component";
 import { StyleSheet } from "react-native";
 import colors from "../../constants/colors";
@@ -8,17 +8,19 @@ type IProps = {
   tabs: { title: string }[];
   selectedTab: string;
   setSelectedTab: (tab: string) => void;
+  style?: ViewStyle;
 };
 export const TabsComponent = ({
   tabs,
   selectedTab,
   setSelectedTab,
+  style,
 }: IProps) => {
   return (
     <ScrollView
       horizontal
       showsHorizontalScrollIndicator={false}
-      contentContainerStyle={propertyDetailsStyle.tabsScrollContainer}
+      contentContainerStyle={[propertyDetailsStyle.tabsScrollContainer, style]}
     >
       {tabs.map((tab) => (
         <TouchableOpacity
