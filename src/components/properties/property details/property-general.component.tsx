7ce167f5a9ece6bf777b05ecd09e9744729fd816
Chiheb Rahmouni <chihebrahmouni32@gmail.com>
2025-08-09 16:07:34 +0100

    feat: Add new icons and enhance PropertyGeneralComponent with detailed property information display

--- src/components/properties/property details/property-general.component.tsx
+++ src/components/properties/property details/property-general.component.tsx
@@ -9,7 +9,16 @@ import { PropertyIndicators } from "../../../components/properties/property-indi
 import { IconLabelValue } from "../../../components/ui/icon-label-value.component";
 import {
   BuildingIcon,
+  CalendarIcon,
   EmailIcon,
+  GroupOfUsersIcon,
+  HashtagIcon,
+  ListIcon,
+  LocationIcon,
+  MapIcon,
+  MediaIcon,
+  MoonIcon,
+  PostIcon,
   PropertyIcon,
   StatusIcon,
   UserIcon,
@@ -31,9 +40,9 @@ export const PropertyGeneralComponent = ({ property }: IProps) => {
           <TextTitle numberOfLines={1}>{property.title}</TextTitle>
           <PropertyLocation address={property.address} />
           <PropertyIndicators property={property} />
-          <TextBody>
+          {/* <TextBody>
             {property.description || "No description available."}
-          </TextBody>
+          </TextBody> */}
         </View>
       </CardComponent>
 
@@ -71,6 +80,81 @@ export const PropertyGeneralComponent = ({ property }: IProps) => {
             icon={<StatusIcon />}
             label="Status"
             value={<Badge type="success" text="Available*" />}
+          />
+        </View>
+      </CardComponent>
+      <CardComponent>
+        <View style={propertyDetailsStyle.section}>
+          <TextTitle
+            numberOfLines={1}
+            style={propertyDetailsStyle.borderBottom}
+          >
+            Address
+          </TextTitle>
+          <IconLabelValue
+            icon={<LocationIcon />}
+            label="Address"
+            value={property.address?.district || "-"}
+          />
+          <IconLabelValue
+            icon={<MapIcon />}
+            label="City"
+            value={property.address?.city || "-"}
+          />
+          <IconLabelValue
+            icon={<MediaIcon />}
+            label="Country"
+            value={property.address?.country || "-"}
+          />
+          <IconLabelValue
+            icon={<PostIcon />}
+            label="Post Code"
+            value={property.address?.zip || "-"}
+          />
+        </View>
+      </CardComponent>
+      <CardComponent>
+        <View style={propertyDetailsStyle.section}>
+          <TextTitle
+            numberOfLines={1}
+            style={propertyDetailsStyle.borderBottom}
+          >
+            Property information
+          </TextTitle>
+          <IconLabelValue
+            icon={<HashtagIcon />}
+            label="Property ID"
+            value={property._id || "-"}
+          />
+          <IconLabelValue
+            icon={<ListIcon />}
+            label="Apartment No"
+            value={property.apartmentNumber || "-"}
+          />
+          <IconLabelValue
+            icon={<BuildingIcon />}
+            label="Building Name"
+            value={property.buildingName || "-"}
+          />
+          <IconLabelValue
+            icon={<MoonIcon />}
+            label="Maximum Stays"
+            value={property.maxNights || "-"}
+          />
+          <IconLabelValue
+            icon={<PropertyIcon />}
+            label="Floor"
+            value={property.floor || "-"}
+          />
+          <IconLabelValue
+            icon={<CalendarIcon />}
+            label="Year Built"
+            value={property.yearBuilt || "-"}
+          />
+          <IconLabelValue
+            icon={<GroupOfUsersIcon />}
+            label="Maximum Capacity"
+            value="2*"
           />
         </View>
       </CardComponent>
