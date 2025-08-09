78fe048e4cdd826dc06c73a7cd59cf69d4893d37
Chiheb Rahmouni <chihebrahmouni32@gmail.com>
2025-08-09 13:23:57 +0100

    feat: Add PropertyDetailsPage and update navigator to set it as initial route

--- src/navigation/Properties-navigator.tsx
+++ src/navigation/Properties-navigator.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { ERoute } from "../utils";
 import { PropertiesListPage } from "../screens/properties/properties list/properties-list.page";
+import { PropertyDetailsPage } from "../screens/properties/property details/property-details.page";
 
 const PropertiesStack = createNativeStackNavigator();
 
@@ -9,11 +10,15 @@ export const PropertiesNavigator = () => {
   return (
     <PropertiesStack.Navigator
       screenOptions={{ headerShown: false }}
-      initialRouteName={ERoute.PROPERTIES_LIST}
+      initialRouteName={ERoute.PROPERTIES_DETAILS}
     >
       <PropertiesStack.Screen
         name={ERoute.PROPERTIES_LIST}
         component={PropertiesListPage}
+      />
+      <PropertiesStack.Screen
+        name={ERoute.PROPERTIES_DETAILS}
+        component={PropertyDetailsPage}
       />
     </PropertiesStack.Navigator>
   );
---
+++ src/screens/properties/property details/property-details.page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export const PropertyDetailsPage = () => {
+  return (
+    <View>
+      <View>
+        <Text>Property Details Page</Text>
+      </View>
+    </View>
+  );
+};
